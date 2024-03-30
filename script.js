bd10d13a99e0faba11ea584dfab06c67b2cc6aca
Vinay <vcvcchaturvedi@gmail.com>
2024-03-31 02:01:36 +0530

    Fix theme time

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const currentTime = new Date();
 const evening = currentTime.getHours() > 18 ? true : false;
 const bodyElem = document.querySelector("body");
 if (evening) {
-  bodyElem.classList.add("light-theme");
-} else {
   bodyElem.classList.add("dark-theme");
+} else {
+  bodyElem.classList.add("light-theme");
 }
 export { bodyElem };
